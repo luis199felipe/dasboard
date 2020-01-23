55f7eafbce5460d5aeb357531c8a262536d7a903
Luis Felipe <felipe141099@gmial.com>
2020-01-23 12:40:04 -0500

    First COmmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TableroComponent } from './tablero/tablero.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '', component: TableroComponent
+  }
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    TableroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
